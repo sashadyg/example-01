e42c79b2a1ac1deb9eb0eaf59f33b8f7b74a00b2
sashadyg <dyg.sasha12@gmail.com>
2022-04-11 16:39:58 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 import './sass/main.scss';
 import { fetchMovies } from './js/fecthMovies';
+import cardHBS from '../src/template/card.hbs'
 
-const ids = [27, 9, 14, 15, 100, 28, 24, 5, 13, 23, 54, 64, 55, 76, 87, 98, 566, 10, 11, 19, 20]
+const ids = [27, 9, 14, 15, 100, 28, 24, 5, 13, 64, 55, 76, 87, 98, 11, 19, 20]
 
 localStorage.setItem("movie-id", JSON.stringify(ids))
 
-// const localStorageId = localStorage.getItem("movie-id")
+const listRef = document.querySelector('.film-card__list')
 
-fetchMovies(574).then(movie => console.log(movie))
+// fetchMovies(574).then(movie => console.log(movie))
 
-const getLocalIdWatched = () => parsedId = JSON.parse(localStorage.getItem("movie-id"))
-
-function checkWatchedList() {
-    if(getLocalIdWatched().length < 0) {
-        return
-    } else if(getLocalIdWatched().length > 20) {
-        
-    }
+function getLocalIdWatched() {
+    const parseId = JSON.parse(localStorage.getItem("movie-id"))
+    return parseId
 }
 
-function markupWatchedMovies(movie) {
-    const markupMovie = movie.map(({ original_title, poster_path, genres, vote_average }) => {
-        const markup = ``
+function markupWatchedMovies() {
+    // e.preventDefault();
+    listRef.innerHTML = '';
+    
+    const localId = getLocalIdWatched()
 
-        return markup
-    }).join('')
-
-    return markupMovie
+    localId.map(id => {
+        fetchMovies(id)
+        .then(movie => {
+            const markup = cardHBS(movie)
+            listRef.insertAdjacentHTML('beforeend', markup)
+        })
+    })
 }
+
+markupWatchedMovies()
--- src/js/fecthMovies.js
+++ src/js/fecthMovies.js
@@ -1,7 +1,7 @@
 const API_KEY = '70c5c640dcd47438a9460ce1b8e1a5b1'
 
 export function fetchMovies(id) {
-    return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+    return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`)
         .then(response => response.json())
         .catch(error => console.log(error))
 }
